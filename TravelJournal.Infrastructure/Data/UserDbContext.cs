d1996c9f7214a31445593c37127803a9826c3e3f
Максим Воскобович <145231589+gloomforge@users.noreply.github.com>
2025-05-11 14:11:56 +0300

    feat(infrastructure): configure user database and implement user rep

---
+++ TravelJournal.Infrastructure/Data/UserDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using TravelJournal.Domain.Models;
+using TravelJournal.Domain.ValueObjects;
+
+namespace TravelJournal.Infrastructure.Data;
+
+public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users");
+            
+            entity.Property(u => u.Id)
+                .HasColumnName("id");
+
+            entity.Property(u => u.Name)
+                .HasColumnName("name");
+            
+            entity.Property(u => u.Email)
+                .HasConversion(vo => vo.Value, str => Email.Create(str))
+                .HasColumnName("email");
+
+            entity.Property(u => u.CreatedAt)
+                .HasColumnName("created_at");
+        });
+    }
+}
